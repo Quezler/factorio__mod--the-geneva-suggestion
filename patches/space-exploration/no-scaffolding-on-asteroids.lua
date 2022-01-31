9211ab8e7372adee1264b64a5178deac7d3212fb
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-01-31 11:12:19 +0100

    Prevent scaffolding from being placed on asteroids

---
+++ patches/space-exploration/no-scaffolding-on-asteroids.lua
@@ -0,0 +1,9 @@
+-- data.lua
+
+if mods["space-exploration"] then
+    local collision_mask_util = require("collision-mask-util")
+    local asteroid_layer = collision_mask_util.get_first_unused_layer()
+
+    table.insert(data.raw["item"]["se-space-platform-scaffold"].place_as_tile.condition, asteroid_layer)
+    table.insert(data.raw["tile"]["se-asteroid"].collision_mask, asteroid_layer)
+end
