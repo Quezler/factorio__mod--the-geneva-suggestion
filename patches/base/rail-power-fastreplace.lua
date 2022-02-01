a9ad1f21dca354d84f0fc54975238983125f0357
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-02-01 18:57:08 +0100

    Allow manually placing substations over big poles

---
+++ patches/base/rail-power-fastreplace.lua
@@ -0,0 +1,6 @@
+-- data.lua
+
+if mods["base"] then
+  data.raw["electric-pole"]["big-electric-pole"].fast_replaceable_group = "rail-power"
+  data.raw["electric-pole"]["substation"].fast_replaceable_group = "rail-power"
+end
