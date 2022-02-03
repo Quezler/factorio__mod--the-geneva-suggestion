d29f2a8e920d9225bf755925239303375c417fe9
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-02-03 09:30:08 +0100

    Reverse beacon animation

---
+++ patches/base/reverse-beacon-animation.lua
@@ -0,0 +1,5 @@
+-- data.lua
+if mods["base"] then
+  data.raw["beacon"]["beacon"].graphics_set.animation_list[3].animation           .run_mode = "backward"
+  data.raw["beacon"]["beacon"].graphics_set.animation_list[3].animation.hr_version.run_mode = "backward"
+end
