3c55bd1e70765c1c3cafa62db0db7ef1121287df
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-02-12 14:46:51 +0100

    Split into active and inactive queue

--- zip/scripts/nuclear-reactor.lua
+++ zip/scripts/nuclear-reactor.lua
@@ -6,19 +6,23 @@ local queue = require("__flib__.queue")
 
 local nuclear_reactor = {}
 
--- local deactivate_above =
--- local reactivate_above =
+local deactivate_above = 900
+local reactivate_under = 900
 
 function nuclear_reactor.init()
   global["nuclear-reactor"] = {}
 
-  global["nuclear-reactor"]["all"] = queue.new()
-  global["nuclear-reactor"]["overheated"] = queue.new()
-  global["nuclear-reactor"]["proxied"] = {}
+  global["nuclear-reactor"]["active"] = queue.new()
+  global["nuclear-reactor"]["inactive"] = queue.new()
+  global["nuclear-reactor"]["deliveries"] = {}
 
   for _, surface in pairs(game.surfaces) do
     for _, entity in pairs(surface.find_entities_filtered{type = "reactor", name = "nuclear-reactor"}) do
-      queue.push_right(global["nuclear-reactor"]["all"], entity)
+      if entity.active then
+        queue.push_right(global["nuclear-reactor"]["active"], entity)
+      else
+        queue.push_right(global["nuclear-reactor"]["inactive"], entity)
+      end
     end
   end
 end
@@ -28,14 +32,20 @@ function nuclear_reactor.on_created_entity(event)
   if not (entity and entity.valid) then return end
 
   if entity.type == "reactor" and entity.name == "nuclear-reactor" then
-    queue.push_left(global["nuclear-reactor"]["all"], entity)
+    if entity.active then
+      game.print("active")
+      queue.push_left(global["nuclear-reactor"]["active"], entity)
+    else
+      game.print("inactive")
+      queue.push_left(global["nuclear-reactor"]["inactive"], entity)
+    end
   end
 end
 
 function nuclear_reactor.on_entity_destroyed(event)
-  if global["nuclear-reactor"]["proxied"][event.registration_number] then
-    local reactor = global["nuclear-reactor"]["proxied"][event.registration_number]
-    global["nuclear-reactor"]["proxied"][event.registration_number] = nil
+  if global["nuclear-reactor"]["deliveries"][event.registration_number] then
+    local reactor = global["nuclear-reactor"]["deliveries"][event.registration_number]
+    global["nuclear-reactor"]["deliveries"][event.registration_number] = nil
 
     if reactor and reactor.valid then
 
@@ -60,36 +70,53 @@ function nuclear_reactor.on_entity_destroyed(event)
   end
 end
 
-function nuclear_reactor.once()
-  local reactor = queue.pop_left(global["nuclear-reactor"]["all"])
+function nuclear_reactor.handle_one_active(reactor)
+  local reactor = queue.pop_left(global["nuclear-reactor"]["active"])
   if not reactor or not reactor.valid then return end
 
-  if not reactor.active and reactor.temperature < 750 then
-    reactor.active = true
-  elseif reactor.active and reactor.temperature > 750 then
+  if reactor.temperature > deactivate_above then
+    queue.push_right(global["nuclear-reactor"]["inactive"], reactor)
     reactor.active = false
-  end
+    --reactor.surface.create_entity{name = "flying-text", position = reactor.position, text = "[virtual-signal=signal-red]"}
+  else
+    queue.push_right(global["nuclear-reactor"]["inactive"], reactor)
+    if reactor.get_inventory(defines.inventory.fuel).get_item_count() == 0 then
 
-  if reactor.get_inventory(defines.inventory.fuel).get_item_count() == 0 then
+      local surrounded_on_all_sides = reactor.neighbours["north"] ~= nil and reactor.neighbours["east"] ~= nil and reactor.neighbours["south"] ~= nil and reactor.neighbours["west"] ~= nil
+      if not surrounded_on_all_sides then
 
-    local surrounded_on_all_sides = reactor.neighbours["north"] ~= nil and reactor.neighbours["east"] ~= nil and reactor.neighbours["south"] ~= nil and reactor.neighbours["west"] ~= nil
-    if not surrounded_on_all_sides then
-
-      if not construction_robot.pending_delivery(reactor) then
-        local proxy = construction_robot.deliver(reactor, {["uranium-fuel-cell"] = 1})
-        global["nuclear-reactor"]["proxied"][script.register_on_entity_destroyed(proxy)] = reactor
+        if not construction_robot.pending_delivery(reactor) then
+          local proxy = construction_robot.deliver(reactor, {["uranium-fuel-cell"] = 1})
+          global["nuclear-reactor"]["deliveries"][script.register_on_entity_destroyed(proxy)] = reactor
+        end
       end
     end
   end
+end
 
-  queue.push_right(global["nuclear-reactor"]["all"], reactor)
+function nuclear_reactor.handle_one_inactive(reactor)
+  local reactor = queue.pop_left(global["nuclear-reactor"]["inactive"])
+  if not reactor or not reactor.valid then return end
+
+  if reactor.temperature < reactivate_under then
+    queue.push_right(global["nuclear-reactor"]["active"], reactor)
+    reactor.active = true
+    --reactor.surface.create_entity{name = "flying-text", position = reactor.position, text = "[virtual-signal=signal-green]"}
+  else
+    queue.push_right(global["nuclear-reactor"]["inactive"], reactor)
+  end
 end
 
 function nuclear_reactor.every_second()
-  -- ensure each reactor is serviced at least once every 100 seconds
-  for i = 0, math.ceil(queue.length(global["nuclear-reactor"]["all"]) / 100), 1 do
-    nuclear_reactor.once()
+  if queue.length(global["nuclear-reactor"]["active"]) > 0 then
+    nuclear_reactor.handle_one_active()
   end
+  if queue.length(global["nuclear-reactor"]["inactive"]) > 0 then
+    nuclear_reactor.handle_one_inactive()
+  end
+
+--   game.print("active: " .. queue.length(global["nuclear-reactor"]["active"]))
+--   game.print("inactive: " .. queue.length(global["nuclear-reactor"]["inactive"]))
 end
 
 
